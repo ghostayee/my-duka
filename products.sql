e77ed87caefab751963b48046d554d28368e9b5f
ghostayee <mayeeayee@outlook.com>
2026-02-12 13:57:25 +0300

    get some data

---
+++ products.sql
@@ -0,0 +1,36 @@
+create table products (
+	id INT,
+	name VARCHAR(50),
+	buying_price VARCHAR(50),
+	selling_price VARCHAR(50)
+);
+insert into products (id, name, buying_price, selling_price) values (1, 'Insulated Lunch Bag', 24.99, 12.99);
+insert into products (id, name, buying_price, selling_price) values (2, 'Whole Grain Cereal', 3.49, 6.99);
+insert into products (id, name, buying_price, selling_price) values (3, 'Blackberry Compote', 5.29, 5.99);
+insert into products (id, name, buying_price, selling_price) values (4, 'Garlic Breadsticks', 3.49, 2.49);
+insert into products (id, name, buying_price, selling_price) values (5, 'Coffee Subscription Service', 29.99, 2.49);
+insert into products (id, name, buying_price, selling_price) values (6, 'Sweet Chili Sauce', 2.99, 5.49);
+insert into products (id, name, buying_price, selling_price) values (7, 'Fleece Hoodie', 39.99, 2.09);
+insert into products (id, name, buying_price, selling_price) values (8, 'Body Pillow Case', 14.99, 8.99);
+insert into products (id, name, buying_price, selling_price) values (9, 'Cranberry Orange Oatmeal', 2.49, 3.49);
+insert into products (id, name, buying_price, selling_price) values (10, 'Noise Cancelling Ear Muffs', 24.99, 4.49);
+insert into products (id, name, buying_price, selling_price) values (11, 'Teriyaki Chicken Wings', 10.99, 22.99);
+insert into products (id, name, buying_price, selling_price) values (12, 'Ceramic Planter Set', 39.99, 3.29);
+insert into products (id, name, buying_price, selling_price) values (13, 'Low-Fat Yogurt', 3.29, 3.99);
+insert into products (id, name, buying_price, selling_price) values (14, 'Travel Jewelry Case', 24.99, 3.49);
+insert into products (id, name, buying_price, selling_price) values (15, 'Bamboo Cotton Tank Top', 22.99, 109.99);
+insert into products (id, name, buying_price, selling_price) values (16, 'Garlic Herb Marinade', 3.29, 79.99);
+insert into products (id, name, buying_price, selling_price) values (17, 'Organic Black Bean Burger', 4.49, 3.49);
+insert into products (id, name, buying_price, selling_price) values (18, 'Leek and Potato Soup', 3.29, 8.49);
+insert into products (id, name, buying_price, selling_price) values (19, 'Gardening Tool Belt', 22.99, 89.99);
+insert into products (id, name, buying_price, selling_price) values (20, 'Weighted Blanket', 79.99, 19.99);
+insert into products (id, name, buying_price, selling_price) values (21, 'Coconut Macaroons', 4.99, 39.99);
+insert into products (id, name, buying_price, selling_price) values (22, 'Travel Orthopedic Pillow', 24.99, 5.99);
+insert into products (id, name, buying_price, selling_price) values (23, 'Peach Mango Smoothie Mix', 5.99, 4.29);
+insert into products (id, name, buying_price, selling_price) values (24, 'Fishing Rod', 49.99, 34.99);
+insert into products (id, name, buying_price, selling_price) values (25, 'Wooden Children''s Play Kitchen', 129.99, 1.5);
+insert into products (id, name, buying_price, selling_price) values (26, 'Safety Goggles', 14.99, 2.09);
+insert into products (id, name, buying_price, selling_price) values (27, 'Pumpkin Spice Cookies', 3.29, 19.99);
+insert into products (id, name, buying_price, selling_price) values (28, 'Kids'' Gardening Kit', 24.99, 24.99);
+insert into products (id, name, buying_price, selling_price) values (29, 'Sweet BBQ Dipping Sauce', 3.49, 3.79);
+insert into products (id, name, buying_price, selling_price) values (30, 'Sliced Cheese', 4.49, 39.99);
